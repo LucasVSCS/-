6c4f7ee1712f3b81d8dbd76d80ff224991e57c25
Lucas Vinicius <lucasvscs@outlook.com>
2019-07-14 18:08:42 -0300

    Menu superior responsivo, imagem central da landing page padronizada, seções: sobre mim e conhecimentos concluidas. Seção de projetos em desenvolvimento

--- js/main.js
+++ js/main.js
@@ -1,5 +1,64 @@
-document.addEventListener("DOMContentLoaded", function () {
-    //The first argument are the elements to which the plugin shall be initialized
-    //The second argument has to be at least a empty object or a object with your desired options
-    OverlayScrollbars(document.querySelectorAll('body'), {});
-});
+/**
+ * Simple preloader in Javascript
+ * 
+ * Copyright (c) 2016 iBird Rose
+ * Dual licensed under the MIT and GPL licenses.
+ *
+ * Version 1.1.1
+ */
+
+/* preloader */
+
+(function () {
+  var preloaderDiv = document.createElement ('div');
+  preloaderDiv.innerHTML = `
+		<style>
+            #preloader_preload {
+                display: block;
+                z-index: 99999;
+                height: 100vh;
+                background: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzgiIGhlaWdodD0iMzgiIHZpZXdCb3g9IjAgMCAzOCAzOCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBzdHJva2U9IiNmZmYiPiAgICA8ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPiAgICAgICAgPGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoMSAxKSIgc3Ryb2tlLXdpZHRoPSIyIj4gICAgICAgICAgICA8Y2lyY2xlIHN0cm9rZS1vcGFjaXR5PSIuNSIgY3g9IjE4IiBjeT0iMTgiIHI9IjE4Ii8+ICAgICAgICAgICAgPHBhdGggZD0iTTM2IDE4YzAtOS45NC04LjA2LTE4LTE4LTE4Ij4gICAgICAgICAgICAgICAgPGFuaW1hdGVUcmFuc2Zvcm0gICAgICAgICAgICAgICAgICAgIGF0dHJpYnV0ZU5hbWU9InRyYW5zZm9ybSIgICAgICAgICAgICAgICAgICAgIHR5cGU9InJvdGF0ZSIgICAgICAgICAgICAgICAgICAgIGZyb209IjAgMTggMTgiICAgICAgICAgICAgICAgICAgICB0bz0iMzYwIDE4IDE4IiAgICAgICAgICAgICAgICAgICAgZHVyPSIxcyIgICAgICAgICAgICAgICAgICAgIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIi8+ICAgICAgICAgICAgPC9wYXRoPiAgICAgICAgPC9nPiAgICA8L2c+PC9zdmc+') center center no-repeat;
+                background-color: #D3D3D3;
+                position: fixed;
+                top: 0;
+                width: 100%;
+                left: 0;
+            }
+
+            body.preloader {
+              overflow: hidden;
+              height: 100vh;
+            }
+        </style>
+        <div id="preloader"><div id="preloader_preload"></div></div>
+	`;
+
+  document.body.insertBefore (preloaderDiv, document.body.firstChild);
+
+  var preloaderStart = document.getElementById ('preloader_preload');
+
+  document.body.classList.add ('preloader');
+
+  function fadeOutFunction (el) {
+    el.style.opacity = 1;
+
+    var preloaderEngine = setInterval (function () {
+      document.body.classList.remove ('preloader');
+
+      el.style.opacity = el.style.opacity - 0.05;
+
+      if (el.style.opacity <= 0.05) {
+        clearInterval (preloaderEngine);
+        preloaderStart.style.display = 'none';
+      }
+    }, 20);
+  }
+
+  window.onload = function () {
+    setTimeout (function () {
+      fadeOutFunction (preloaderStart);
+    }, 1000);
+  };
+}) ();
+
+/* END preloader */
